4ba5aeb581b3ef96ab9b27223c77c2a37408f93a
n0lley <ntolley13@gmail.com>
2022-09-11 22:32:48 -0400

    narrowed slowdown to robot.think(). Removed extra timers in preparation to add more specific ones.

--- simulate.py
+++ simulate.py
@@ -4,7 +4,7 @@ import time
 from simulation import SIMULATION
 
 t0 = time.time()
-simulation = SIMULATION(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+simulation = SIMULATION(sys.argv[1], sys.argv[2], sys.argv[3])
 simulation.run(int(sys.argv[2]), sys.argv[3], sys.argv[4])
 f = open(sys.argv[4] + "_individual_simtime.txt", "a")
 f.write(str(time.time() - t0) + "\n")
